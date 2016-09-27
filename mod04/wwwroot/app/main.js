dd0964af067d97cc6e8e55e810623753c132726b
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-27 10:53:10 +0200

    After demo

--- mod04/wwwroot/app/main.js
+++ mod04/wwwroot/app/main.js
@@ -1,5 +1,26 @@
 (function(){
 
+    angular.module('myApp', [])
+        .service('CatsService', function() {
+                    return {
+                        getData: function(){
+                            return {
+                                name: 'Diego'
+                            };
+                        }
+                    }
+        })
+        .controller('DemoController', ['$scope', 'CatsService', function(a, b) {
+            console.log('In the controller');
 
+            a.cat = b.getData();
+
+            a.cats = [
+                 a.cat,
+                {
+                    name: 'Zorro'
+                },
+            ]
+        }])
 
 }());
