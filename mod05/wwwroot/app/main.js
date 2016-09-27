0ed141d07d3b292756d4c493be579f547b1d8067
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-27 14:32:22 +0200

    After demo

--- mod05/wwwroot/app/main.js
+++ mod05/wwwroot/app/main.js
@@ -2,6 +2,20 @@
 
     angular
         .module('myApp', [])
-        
+        .directive('myDemo', function(){
+
+            return {
+                restrict: 'AE',
+                transclude: true,
+                scope: true,
+                templateUrl: '/app/my-demo.html',
+                link:  function(scope, element){
+                    console.log('myDemo is linking');
+                    element.bind('click', function(){
+                        console.log('I was clicked');
+                    })
+            }
+            }
+        })
 
 }());
