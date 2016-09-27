d9f4e4b27b4f6fb5b6f7364ec61e406172a95b3f
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-27 16:35:49 +0200

    Demo code

--- mod06/wwwroot/app/main.js
+++ mod06/wwwroot/app/main.js
@@ -2,6 +2,15 @@
 
     angular
         .module('myApp', [])
+        .controller('MoviesController', function($http, $scope){
+
+            $http.get('api/movies')
+                .then(function(rsp) { 
+                    $scope.movies = rsp.data;
+                    console.table(rsp.data)
+                })
+
+        })
         
 
 }());
