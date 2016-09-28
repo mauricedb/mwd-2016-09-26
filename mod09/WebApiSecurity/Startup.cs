bfdb679f915f03e4c51144f6fd71d614b844e377
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-28 07:51:19 +0200

    Before demo

---
+++ mod09/WebApiSecurity/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApiSecurity.Startup))]
+
+namespace WebApiSecurity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+        }
+    }
+}
