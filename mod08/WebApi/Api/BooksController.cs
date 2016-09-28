4e6ba59ad8659409a94160e94f36c1187409c73f
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-28 13:06:10 +0200

    After demo

---
+++ mod08/WebApi/Api/BooksController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Api
+{
+    public class BooksController: ApiController
+    {
+
+        private IBooksRepository _repo;
+
+        public BooksController():this(new BooksRepository())
+        {
+        }
+
+        public BooksController(IBooksRepository repo)
+        {
+            _repo = repo;
+        }
+
+        //public Task<IEnumerable<Book>>  Get()
+        //{
+        //    return _repo.GetBooks();
+        //}
+
+        public async Task<IEnumerable<Book>> Get()
+        {
+            var books = await _repo.GetBooks();
+
+            return books;
+        }
+
+        public Task<Book> Get(int id)
+        {
+            return _repo.GetBook(id);
+        }
+    }
+}
