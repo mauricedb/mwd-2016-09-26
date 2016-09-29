6a099e2e623dc4388073c6b3b03dbec5cbf72432
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-29 14:25:36 +0200

    After demo

--- mod11/wwwroot/app/main.js
+++ mod11/wwwroot/app/main.js
@@ -5,7 +5,24 @@
         .controller('CatsController', function($scope) {
             $scope.cats = [];
             $scope.newCat = '';
-            
+
+            // if (sessionStorage.cats){
+            //     $scope.cats = JSON.parse(sessionStorage.cats);
+            // }
+            // for(var i = 0 ; i< localStorage.length; i++){
+            //     try {
+            //         $scope.cats.push(JSON.parse(localStorage[localStorage.key(i)]));
+            //     }
+            //     catch (err) {
+            //         console.error(err);
+            //     }
+            // }
+            localforage.iterate(function(cat){
+                 $scope.cats.push(cat);
+            }).then(function(){
+                $scope.$apply();
+            })
+
             $scope.add = function() {
                 var cat = {
                     name: $scope.newCat
@@ -13,6 +30,11 @@
             
                 $scope.cats.push(cat);
                 $scope.newCat = '';
+
+                // localStorage[cat.name] = JSON.stringify(cat);
+                // sessionStorage.cats= JSON.stringify($scope.cats);
+
+                localforage.setItem(cat.name, cat);
             }
         });
 
