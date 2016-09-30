099783071b43ddcb55c059b8a11ea80e7056781d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-30 16:26:15 +0200

    Angular 2 demo

---
+++ mod16/demo/src/app/demo2/demo2.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { Demo2Component } from './demo2.component';
+
+describe('Component: Demo2', () => {
+  it('should create an instance', () => {
+    let component = new Demo2Component();
+    expect(component).toBeTruthy();
+  });
+});
